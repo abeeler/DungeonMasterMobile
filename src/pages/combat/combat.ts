4ac7bb9a610563ab5e30e33460b87ed2a28c8198
A. Beeler <abeeler.cs@gmail.com>
2018-06-22 20:31:33 -0400

    Sort combatants by their initiative
    The combat order will be listed top to bottom as a result

--- src/pages/combat/combat.ts
+++ src/pages/combat/combat.ts
@@ -21,7 +21,21 @@ export class CombatPage {
   }
 
   insertEntry(combatant : Combatant) {
-    this.combatants.push(combatant);
+    // If no combatants, push immediately
+    if (this.combatants.length == 0) {
+      this.combatants.push(combatant);
+    } else {
+      // Otherwise loop over each combatant to find sorted position
+      for (var index = 0; index < this.combatants.length; index++) {
+        if (this.combatants[index].initiative < combatant.initiative) {
+          this.combatants.splice(index, 0, combatant);
+          return;
+        }
+      }
+      
+      // If no saved combatants had a lower initiative, add the new one to the end
+      this.combatants.push(combatant);
+    }
   }
 }
 
