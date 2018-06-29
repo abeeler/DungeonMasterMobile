5044a06469bf1390d616e3bbcc504d293c718695
A. Beeler <abeeler.cs@gmail.com>
2018-06-28 21:28:10 -0400

    Persist character data between sessions
    Persisted data will be updated when a new character is added and when
    the CharacterList page is returned to from a character detail page

--- src/pages/character-list/character-list.ts
+++ src/pages/character-list/character-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import { NavController, ModalController } from 'ionic-angular';
 import { Character, CharacterDetailPage } from '../character-detail/character-detail';
 import { Health } from '../combat/combat';
@@ -9,22 +10,33 @@ import { CharacterEntryModal } from '../character-entry/character-entry';
   templateUrl: 'character-list.html'
 })
 export class CharacterListPage {
+  static readonly STORED_CHARACTERS = 'characters';
   static readonly CHARACTER_PARAM = 'character';
 
+  loaded: boolean;
   characters : Character[];
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+  constructor(public storage: Storage, public navCtrl: NavController, public modalCtrl: ModalController) {
+    this.loaded = false;
     this.characters = [];
-    this.characters.push(new Character({
-      name: "Johnson Jacobs",
-      statistics: [6, 10, 14, 15, 9, 8],
-      savingThrows: [2, 5],
-      health: new Health(15),
-      currentHitDie: 1,
-      maxHitDie: 1,
-      armorClass: 11,
-      proficiencies: [2, 4, 8]
-    }));
+    storage.get(CharacterListPage.STORED_CHARACTERS).then((characters : Character[]) => {
+      if (characters) {
+        for(let character of characters) {
+          this.characters.push(new Character(character));
+        }
+      } else {
+        this.characters.push(new Character({
+          name: "Johnson Jacobs",
+          statistics: [6, 10, 14, 15, 9, 8],
+          savingThrows: [2, 5],
+          health: new Health(15),
+          currentHitDie: 1,
+          maxHitDie: 1,
+          armorClass: 11,
+          proficiencies: [2, 4, 8]
+        }));
+      }
+    });
   }
 
   presentCharacterEntryModal() {
@@ -32,6 +44,7 @@ export class CharacterListPage {
     entryModal.onDidDismiss((data: Character) => {
       if (data) {
         this.characters.push(data);
+        this.storage.set(CharacterListPage.STORED_CHARACTERS, this.characters);
       }
     });
 
@@ -45,4 +58,12 @@ export class CharacterListPage {
     data[CharacterListPage.CHARACTER_PARAM] = character;
     this.navCtrl.push(CharacterDetailPage, data);
   }
+
+  ionViewWillEnter() {
+    if (!this.loaded) {
+      this.loaded = true;
+    } else {
+      this.storage.set(CharacterListPage.STORED_CHARACTERS, this.characters);
+    }
+}
 }
