00a627ab518b1f78d80f6d8b60a6c91bb5cd1527
A. Beeler <abeeler.cs@gmail.com>
2018-07-05 16:25:55 -0400

    Replace character entry headers with a segment
    Ports the functionality of the CharacterDetailsPage segment and slides
    over into the CharacterEntryPage

--- src/pages/character-entry/character-entry.ts
+++ src/pages/character-entry/character-entry.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { ViewController, AlertController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { ViewController, AlertController, NavParams, Slides } from 'ionic-angular';
 import { CharacterListPage } from '../character-list/character-list';
 import { Character, SimpleCharacter, PlayerCharacter } from '../../classes/character';
 import { CharacterProvider } from '../../providers/character/character';
@@ -9,6 +9,9 @@ import { CharacterProvider } from '../../providers/character/character';
   templateUrl: 'character-entry.html'
 })
 export class CharacterEntryModal {
+  @ViewChild(Slides) slides: Slides;
+  
+  currentSegment: string;
   originatingCharacter: SimpleCharacter;
   character: Character;
 
@@ -17,6 +20,7 @@ export class CharacterEntryModal {
       public params: NavParams,
       public viewCtrl: ViewController,
       public alertCtrl: AlertController) {
+    this.currentSegment = "0";
     this.character = new PlayerCharacter();
     this.originatingCharacter = params.get(CharacterListPage.CHARACTER_PARAM);
     if (this.originatingCharacter) {
@@ -101,5 +105,16 @@ export class CharacterEntryModal {
       }
     });
     alert.present();
+  }
+
+  slideChanged() {
+    let activeIndex = this.slides.getActiveIndex();
+    if (activeIndex > 3) return;
+    this.currentSegment = activeIndex + "";
+  }
+
+  segmentChange(event) {
+    let slideIndex = Math.floor(event.value);
+    this.slides.slideTo(slideIndex, 500);
   }
 }
