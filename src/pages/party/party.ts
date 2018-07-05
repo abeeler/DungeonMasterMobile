05a155b87b41bd249d7781ead3ba6574b6d59938
A. Beeler <abeeler.cs@gmail.com>
2018-07-05 14:22:20 -0400

    Track party members by character id
    Duplicate members are now prevented, with a toast message popping up to
    warn the user if they try to add someone already in the party.
    
    Party member data is now loaded using the CharacterProvider rather than
    trying to reinitialize from the stored JSON object, fixing issues caused
    by difference between Character and PlayerCharacter.
    
    Resolves #3

--- src/pages/party/party.ts
+++ src/pages/party/party.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { CharacterListPage } from '../character-list/character-list';
-import { Character } from '../../classes/character';
+import { Character, SimpleCharacter } from '../../classes/character';
 import { CharacterDetailPage } from '../character-detail/character-detail';
 import { CombatPage } from '../combat/combat';
+import { CharacterProvider } from '../../providers/character/character';
 
 @Component({
   selector: 'page-party',
@@ -14,13 +15,21 @@ export class PartyPage {
   static readonly STORED_MEMBERS = 'members';
 
   members: Character[];
+  memberIDs: number[];
 
-  constructor(public navCtrl: NavController, public storage: Storage) {
+  constructor(
+      public navCtrl: NavController,
+      public storage: Storage,
+      public characterProvider: CharacterProvider,
+      public toastCtrl: ToastController) {
     this.members = [];
-    storage.get(PartyPage.STORED_MEMBERS).then((characters: Character[]) => {
-      if (characters) {
-        for(let character of characters) {
-          this.members.push(new Character(character));
+    this.memberIDs = [];
+    storage.get(PartyPage.STORED_MEMBERS).then((memberIDs: number[]) => {
+      if (memberIDs) {
+        for(let characterID of memberIDs) {
+          this.memberIDs.push(characterID);
+          characterProvider.getCharacterDetails(characterID)
+            .then(character => this.members.push(character));
         }
       }
     });
@@ -28,9 +37,22 @@ export class PartyPage {
 
   presentCharacterList() {
     let data = {};
-    data[CharacterListPage.CALLBACK_PARAM] = (character: Character) => {
-      this.members.push(character);
-      this.saveParty();
+    data[CharacterListPage.CALLBACK_PARAM] = (character: SimpleCharacter) => {
+      if (this.memberIDs.indexOf(character.id) === -1) {
+        this.characterProvider.getCharacterDetails(character.id)
+          .then(character => {
+            this.members.push(character);
+            this.memberIDs.push(character.id);
+            this.saveParty();
+          });
+      } else {
+        let toast = this.toastCtrl.create({
+          message: `${character.name} is already in the party!`,
+          duration: 2000,
+          position: 'bottom'
+        });
+        toast.present();
+      }
     };
 
     this.navCtrl.push(CharacterListPage, data);
@@ -43,12 +65,13 @@ export class PartyPage {
   }
 
   removeMember(index) {
-    this.members.splice(index, 1);
+    let removed: Character = this.members.splice(index, 1)[0];
+    this.memberIDs.splice(this.memberIDs.indexOf(removed.id), 1);
     this.saveParty();
   }
 
   saveParty() {
-    this.storage.set(PartyPage.STORED_MEMBERS, this.members);
+    this.storage.set(PartyPage.STORED_MEMBERS, this.memberIDs);
   }
 
   startCombat() {
